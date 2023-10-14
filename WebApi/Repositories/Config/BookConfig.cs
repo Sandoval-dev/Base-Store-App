6ac88de854b700dced3910dbf3a2aaa9f7d0c1cc
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2023-10-14 15:07:28 +0300

    Add project files.

---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Bursa'dan Anılar", Price = 75 },
+                new Book { Id = 2, Title = "Para Nasıl Kazanılır?", Price = 750 },
+                new Book { Id = 3, Title = "Oyun Oynamayı Öğren", Price = 142 }
+                );
+        }
+    }
+}
---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+using WebApi.Repositories.Config;
+
+namespace WebApi.Repositories
+{
+    public class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions options):base(options)
+        {
+            
+        }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig()); //modelBuilder üzerinden bir config uygula dedik.
+        }
+    }
+}
